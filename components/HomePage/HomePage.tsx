d42d861c7ee073a9376bbece4f5b9534c6605b63
Abdullah <abdulhhany69@gmail.com>
2026-04-15 14:25:28 +0200

    Add WebPlans component to HomePage for enhanced service offerings display

--- components/HomePage/HomePage.tsx
+++ components/HomePage/HomePage.tsx
@@ -1,12 +1,12 @@
-import Hero from "./Hero/Hero";
-
- 
+import Hero from "./Hero/Hero"
+import WebPlans from "./WebPlans/WebPlans"
 
 const HomePage = () => {
   console.log("HomePage is rendering");
   return (
     <div>
         <Hero />
+        <WebPlans />
     </div>
   )
 }
