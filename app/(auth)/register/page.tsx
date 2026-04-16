beb6ff158186aa0e50d34370fb2845682dbd68ae
Abdullah <abdulhhany69@gmail.com>
2026-04-16 07:19:20 +0200

    Add RegisterForm component and update RegisterPage for user registration

--- app/(auth)/register/page.tsx
+++ app/(auth)/register/page.tsx
@@ -1,8 +1,14 @@
+import RegisterForm from "./RegisterForm";
 
-const page = () => {
+const RegisterPage = () => {
   return (
-    <div>page</div>
-  )
-}
+    <section className="mt-8 flex items-center justify-center">
+      <div className="m-auto bg-white rounded-lg p-5 w-full md:w-2/3">
+        <h1 className="text-3xl font-bold text-gray-800 mb-5">Register Page</h1>
+        <RegisterForm />
+      </div>
+    </section>
+  );
+};
 
-export default page
+export default RegisterPage;
